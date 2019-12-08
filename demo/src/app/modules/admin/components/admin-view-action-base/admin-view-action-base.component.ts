7e7485d35b2883ccd88967cfed12f29dfb5ce745
Atles <etienne.debard@etu.univ-lyon1.fr>
2019-12-08 19:02:14 +0100

    added delete to view action

--- demo/src/app/modules/admin/components/admin-view-action-base/admin-view-action-base.component.ts
+++ demo/src/app/modules/admin/components/admin-view-action-base/admin-view-action-base.component.ts
@@ -5,7 +5,8 @@ import {AdminifyEntityService, EntityViewConfigs, EntityViewConfigsToken} from '
 import {RouteData, RouteParam, RoutePropertySnapshot} from '@ngx-adminify/router';
 import {Observable} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
-import {map, switchMap} from 'rxjs/operators';
+import {switchMap} from 'rxjs/operators';
+import {DialogService} from '../../../../dialog/dialog.service';
 
 @Component({
     selector: 'demo-admin-view-action-base',
@@ -20,7 +21,9 @@ export class AdminViewActionBaseComponent extends AdminActionBaseComponent imple
                 @Inject(RoutePropertySnapshot('action')) actionData: string,
                 public route: ActivatedRoute,
                 @Inject(EntityViewConfigsToken) public viewConfigs: EntityViewConfigs,
-                @Inject(RouteParam('id')) public id: Observable<string>) {
+                @Inject(RouteParam('id')) public id: Observable<string>,
+                protected dialog: DialogService
+    ) {
         super(admin, action, entity);
     }
 
@@ -28,4 +31,12 @@ export class AdminViewActionBaseComponent extends AdminActionBaseComponent imple
         this.entityValue = this.id.pipe(switchMap(id => this.entity.get(+id)));
     }
 
+    onDelete(row: any) {
+        this.dialog.warn('Delete', 'Are you sure ?', 'delete')
+            .subscribe(result => {
+                if (result) {
+                    this.entity.delete(row.id);
+                }
+            });
+    }
 }
